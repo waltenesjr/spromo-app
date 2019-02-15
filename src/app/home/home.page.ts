5fef311dd5a8c5f8afa844d350fa4d896981f145
waltenesjr <waltenesjr@live.com>
2019-02-15 15:17:35 -0200

    inclusão do plugin da camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import {Component, Injector} from '@angular/core';
 import {EstabelecimentoService} from '../service/estabelecimento.service';
 import {ValueModel} from '../model/value.model';
 import {PontoService} from '../service/ponto.service';
+import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
 
 @Component({
     selector: 'app-home',
@@ -18,7 +19,7 @@ export class HomePage {
     private estabelecimentoService: EstabelecimentoService;
     private pontoService: PontoService;
 
-    constructor(private injector: Injector) {
+    constructor(private injector: Injector, private camera: Camera) {
         this.estabelecimentoService = this.injector.get(EstabelecimentoService);
         this.pontoService = this.injector.get(PontoService);
         this.allFornecedores();
@@ -34,6 +35,23 @@ export class HomePage {
     allPontos(): void {
         this.pontoService.all().subscribe((res: ValueModel[]) => {
             this.pontos = res;
+        });
+    }
+
+    getPhoto() {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.FILE_URI,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        };
+
+        this.camera.getPicture(options).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64 (DATA_URL):
+            let base64Image = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+            // Handle error
         });
     }
 }
